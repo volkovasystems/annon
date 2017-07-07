633440cfeb0af1d9b07a6994f73f3117875535ab
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 18:39:49 +0800

    Updated server/client test.

--- annon-test.js
+++ annon-test.js
@@ -3,9 +3,13 @@ const assert = require( "assert" );
 const annon = require( "./annon.js" );
 
 assert.equal( annon( function( ){ } ), true, "should be true" );
-assert.equal( annon( function hello( ){ } ), false, "should be false" );
-assert.equal( annon( "test" ), false, "should be false" );
-assert.equal( annon( "123" ), true, "should be true" );
+
+assert.equal( annon( ( ) => { } ), true, "should be true" );
+
 assert.equal( annon( "" ), true, "should be true" );
+
+assert.equal( annon( function hello( ){ } ), false, "should be false" );
+
+assert.equal( annon( "test" ), false, "should be false" );
 
 console.log( "ok" );
