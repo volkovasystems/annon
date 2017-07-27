69e1860bbdaa328231bebc15f7addd7efeb43fbe
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 15:09:46 +0800

    Updated test.

--- annon-test.js
+++ annon-test.js
@@ -2,14 +2,14 @@
 const assert = require( "assert" );
 const annon = require( "./annon.js" );
 
-assert.equal( annon( function( ){ } ), true, "should return true" );
+assert.equal( annon( function( ){ } ), true, "should be equal to true" );
 
-assert.equal( annon( ( ) => { } ), true, "should return true" );
+assert.equal( annon( ( ) => { } ), true, "should be equal to true" );
 
-assert.equal( annon( "" ), true, "should return true" );
+assert.equal( annon( "" ), true, "should be equal to true" );
 
-assert.equal( annon( function hello( ){ } ), false, "should return false" );
+assert.equal( annon( function hello( ){ } ), false, "should be equal to false" );
 
-assert.equal( annon( "test" ), false, "should return false" );
+assert.equal( annon( "test" ), false, "should be equal to false" );
 
 console.log( "ok" );
